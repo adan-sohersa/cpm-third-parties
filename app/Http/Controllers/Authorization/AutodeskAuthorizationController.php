437681b2f791db73841e87ab6f66591c9ac23081
SmartCoffee-Dev <smartcoffee.dev@gmail.com>
2024-03-22 17:38:06 -0600

    ApsAuthorization => Creating Authorizations using the Autodesk Platform Services.

---
+++ app/Http/Controllers/Authorization/AutodeskAuthorizationController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers\Authorization;
+
+use App\Enums\Authorization\ThirdPartyAuthorizables;
+use App\Http\Controllers\Controller;
+use App\Models\Authorization;
+use App\Source\AutodeskPlatformServices\ApsAuthentication;
+use App\Source\AutodeskPlatformServices\ApsToken;
+use Exception;
+use Illuminate\Http\Request;
+
+class AutodeskAuthorizationController extends Controller
+{
+	/**
+	 * Display a listing of the resource.
+	 */
+	public function index(Request $request)
+	{
+		if (!$request->has('code') || empty($request->code)) {
+			throw new Exception('No authorization code was provided by the provider');
+		}
+
+		if (!$request->has('state') || empty($request->state)) {
+			throw new Exception('No state was provided');
+		}
+
+		// APS_TOKEN
+		// Code
+		$tokenResponse = ApsAuthentication::getTokenByAuthorizationCode($request->code);
+		// Instance
+		$apsAuthorization = new ApsToken(
+			$tokenResponse->json()
+		);
+
+		// AUTHORIZATION
+		// State
+		$requestState = json_decode(urldecode($request->state));
+		$authorizable_type = ThirdPartyAuthorizables::from($requestState->authorizable_type);
+		$authorizable_id = $requestState->authorizable_id;
+		// Virtual instance
+		$authorizationArray = $apsAuthorization
+			->convertToInternal(authorizableType: $authorizable_type, authorizableId: $authorizable_id)
+			->toArray();
+		// Verification fields
+		$fieldsToVerify = array_filter(
+			$authorizationArray,
+			fn ($key) => in_array(needle: $key, haystack: Authorization::UNIQUE_KEYS)
+		);
+		// Persisted instance
+		Authorization::updateOrCreate(
+			$fieldsToVerify,
+			$authorizationArray
+		);
+
+		return redirect()
+			->route(
+				route: 'authorizations.all',
+				parameters: [
+					'type' => $authorizable_type,
+					'authorizable' => $authorizable_id
+				]
+			);
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 */
+	public function store(Request $request)
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 */
+	public function show(string $id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 */
+	public function edit(string $id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 */
+	public function update(Request $request, string $id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 */
+	public function destroy(string $id)
+	{
+		//
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Authorization\AutodeskAuthorizationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
+
+Route::get(uri: '/providers/autodesk/callback', action: [AutodeskAuthorizationController::class, 'index'])->name(name: 'authorizations.aps.callback');
