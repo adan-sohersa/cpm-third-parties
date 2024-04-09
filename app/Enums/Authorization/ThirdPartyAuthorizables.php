c260fe3c869ff0ac8c2b61876a2d66526905a611
SmartCoffee-Dev <smartcoffee.dev@gmail.com>
2024-04-08 18:15:12 -0600

    ThirdPartyAuthorizablez => Defining complementary enums such as the connection and the model identifier ones.

--- app/Enums/Authorization/ThirdPartyAuthorizables.php
+++ app/Enums/Authorization/ThirdPartyAuthorizables.php
@@ -2,8 +2,24 @@
 
 namespace App\Enums\Authorization;
 
+
 enum ThirdPartyAuthorizables: string
 {
+
 	case PROJECT = 'projects';
 	case USER = 'users';
+}
+
+enum AuthorizableConnection: string
+{
+
+	case projects = 'projects_app_db';
+
+	case users = 'authentication_app_db';
+}
+
+enum AuthorizableIdentifier: string 
+{
+
+	case projects = 'id';
 }
