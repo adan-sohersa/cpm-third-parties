09bcd1eaf224039bac38b182b16ffa70c8cb21c6
SmartCoffee-Dev <smartcoffee.dev@gmail.com>
2024-06-23 15:46:31 -0600

    `AuthorizationCard` => Abstracting the AuthorizationCard in order to centralize its state.

--- resources/js/src/authorizations/infraestructure/components/react/AuthorizationCards.tsx
+++ resources/js/src/authorizations/infraestructure/components/react/AuthorizationCards.tsx
@@ -1,5 +1,5 @@
 import { Authorization, EcosystemModule } from "@/types";
-import { Anchor } from "@adan-sohersa/dummy-design-system";
+import { AuthorizationCard } from "./AuthorizationCard";
 
 interface AuthorizationCardsProps {
 	authorizations?: Authorization[],
@@ -18,58 +18,13 @@ const AuthorizationCards: (props: AuthorizationCardsProps) => JSX.Element = (pro
 	}
 
 	return (
-		<div>
+		<>
 			{
 				authorizations.map(authorization => (
-					<article key={authorization.id} className="pb-6 last-of-type:pb-2">
-
-						<div className="w-full flex gap-2">
-
-							<img className="h-14 w-14 rounded-full object-cover" src={authorization.pictureAtProvider} alt={`Picture of ${authorization.usernameAtProvider} in ${authorization.provider}`} />
-
-							<div>
-								<p className="font-base text-large">{authorization.usernameAtProvider}</p>
-								<p className="font-light text-small">{authorization.scopes}</p>
-								<p className="font-light text-small flex gap-1">Status:
-									<span className={`text-${authorization.active ? 'success' : 'danger'}`}>
-										{authorization.active ? 'Active' : 'Inactive'}
-									</span>
-								</p>
-							</div>
-
-						</div>
-
-						<div className="grid grid-cols-5 gap-1 py-2">
-							{
-								modules.map(module => {
-
-									const redirectionUrl = module.redirectionTemplate
-										.replace('{authorizationId}', authorization.id)
-										.replace('{domain}', import.meta.env.VITE_ECOSYSTEM_DOMAIN);
-
-									return (<Anchor
-										className="capitalize"
-										href={redirectionUrl}
-										target="_blank"
-										key={module.name}
-									>
-										<span className="flex items-center gap-1.5">
-											<img
-												className="w-5 h-5 rounded-full"
-												src={module.icon}
-											/>
-											{module.name}
-										</span>
-									</Anchor>)
-								})
-							}
-						</div>
-
-
-					</article>
+					<AuthorizationCard key={authorization.id} authorization={authorization} modules={modules} />
 				))
 			}
-		</div>
+		</>
 	)
 
 }
