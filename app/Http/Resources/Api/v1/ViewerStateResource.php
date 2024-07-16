d7a13b3077a6193e6ac6de7aa7c2f06286bb51e0
SmartCoffee-Dev <smartcoffee.dev@gmail.com>
2024-07-15 18:53:41 -0600

    `ViewerStateResource` => Defining the Api resources related to the ViewerState entity.

---
+++ app/Http/Resources/Api/v1/ViewerStateResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Api\v1;
+
+use App\Http\Resources\AuthorizationResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ViewerStateResource extends JsonResource
+{
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function toArray(Request $request): array
+	{
+		return [
+			'id' => $this->id,
+			'name' => $this->name,
+			'version' => $this->version,
+			'authorizationId' => $this->authorization_id,
+			'state' => json_decode(json: $this->state, associative: true),
+			'authorization' => new AuthorizationResource(resource: $this->whenLoaded('authorization')),
+			'created_at' => $this->created_at,
+			'updated_at' => $this->updated_at,
+		];
+	}
+}
